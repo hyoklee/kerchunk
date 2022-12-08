7e8cacd20b246222839d8f1cad8224bdb1bfb2ee
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2022-12-08 13:38:27 -0600

    ci: add .

--- ATL08.py
+++ ATL08.py
@@ -3,7 +3,7 @@ import fsspec
 import time
 so = dict(anon=True, default_fill_cache=False, default_cache_type='first')
 singles = []
-f = 'data/ATL08_20181014084920_02400109_003_01.h5'
+f = './data/ATL08_20181014084920_02400109_003_01.h5'
 
 with fsspec.open(f, **so) as inf:
      start = time.time()
