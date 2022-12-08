8e5598496664fb685bc19ace1f0f6d8489c44523
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2022-12-08 13:34:31 -0600

    test: fix 'u' error

--- ATL08.py
+++ ATL08.py
@@ -1,10 +1,14 @@
 import kerchunk.hdf
 import fsspec
-
+import time
 so = dict(anon=True, default_fill_cache=False, default_cache_type='first')
 singles = []
 f = 'data/ATL08_20181014084920_02400109_003_01.h5'
+
 with fsspec.open(f, **so) as inf:
-     h5chunks = kerchunk.hdf.SingleHdf5ToZarr(inf, u, inline_threshold=100)
+     start = time.time()
+     h5chunks = kerchunk.hdf.SingleHdf5ToZarr(inf, f, inline_threshold=100)
+     end = time.time()
+     print(end - start)
      singles.append(h5chunks.translate())
 
