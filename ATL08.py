0064c99d846f49f94be3fa5dca0f515389ca8d7c
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2022-12-08 13:53:13 -0600

    test: write json

--- ATL08.py
+++ ATL08.py
@@ -1,14 +1,17 @@
 import kerchunk.hdf
 import fsspec
 import time
+import ujson
+
 so = dict(anon=True, default_fill_cache=False, default_cache_type='first')
-singles = []
 f = './data/ATL08_20181014084920_02400109_003_01.h5'
+fs2 = fsspec.filesystem('')  #local file system to save final jsons to
 
 with fsspec.open(f, **so) as inf:
      start = time.time()
      h5chunks = kerchunk.hdf.SingleHdf5ToZarr(inf, f, inline_threshold=100)
      end = time.time()
+     outf = 'ATL08.json'
+     with fs2.open(outf, 'wb') as f:
+         f.write(ujson.dumps(h5chunks.translate()).encode());     
      print(end - start)
-     singles.append(h5chunks.translate())
-
