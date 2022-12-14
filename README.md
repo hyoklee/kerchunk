bb50c69994225a5d7cfe9345f1679956106cba9f
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2022-12-14 08:34:54 -0600

    test: add Pydap tester

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# kerchunk
-Test Kerchunk
+# Kerchunk
+
+  Test Kerchunk against DMR++.
+
+* [o.py](o.py): Read all DMR++ datasets from sample NASA test files.
+
---
+++ o.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###########################################################################
+# Copyright (C) 2022 The HDF Group
+#
+# Author: Hyo-Kyung Lee (hyoklee@hdfgroup.org)
+#
+# Last Update: 2022/12/14
+###########################################################################
+import os
+import glob
+from pydap.client import open_url
+
+for f in sorted(glob.glob('/tmp/dmrpp/*.dmrpp')):
+    url = 'http://localhost:8080/opendap/' + os.path.basename(f)
+    print(url)
+    dataset = open_url(url)
+    print(dataset)
+    for i in dataset.keys():
+        print(i)
+        d = dataset[i][:]
+        print(d)
+
+
+
