da8cacb43e2ab8b79aaf8979e238194b76e38794
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2022-12-16 16:18:09 -0600

    test: add a workaround for group checking

--- zgroup.py
+++ zgroup.py
@@ -21,18 +21,21 @@ import fsspec
 import glob
 import zarr
 
+# DataTree gives errors during conversion for this HDF5 file.
+skip = ['ATL08_20181014084920_02400109_003_01.h5',
+        'SMAP_L3_SM_P_20150406_R14010_001.h5.json']
+
+# Use the following command's output for the problematic files.
+#
+# $ h5ls -r ATL08_20181014084920_02400109_003_01.h5 | grep Group | cut -d ' ' -f1
+# Compare it with the following function's output.
 def print_visitor(obj):
     if type(obj) == zarr.hierarchy.Group:
         a = obj.name
         print(a)
 
-# DataTree gives Segmentation Fault during conversion for this HDF5 file.
-skip = ['SMAP_L3_SM_P_20150406_R14010_001.h5.json']
-
 for f in sorted(glob.glob('*5.json')):
     print(f)
-    if f in skip:
-        continue    
     mapper = fsspec.get_mapper(
         'reference://',
         fo=f,
@@ -40,7 +43,9 @@ for f in sorted(glob.glob('*5.json')):
         remote_protocol='file',
     )
     za = zarr.open(mapper, mode='r')
-    # za.visitvalues(print_visitor)
+    if f in skip:
+        za.visitvalues(print_visitor)
+        continue
 
     fb = f[:-5]+'.zarr'
     print(fb)
