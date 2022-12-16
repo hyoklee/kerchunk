98295675784a2d1c94348664775fc0d43134ad04
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2022-12-16 12:23:01 -0600

    test: use all files

--- o.py
+++ o.py
@@ -5,23 +5,35 @@
 #
 # Author: Hyo-Kyung Lee (hyoklee@hdfgroup.org)
 #
-# Last Update: 2022/12/14
+# Last Update: 2022/12/16
 ###########################################################################
+
+"""
+Read all datasets from all files using DMR++.
+"""
+
 import os
 import glob
 from pydap.client import open_url
 
-for f in sorted(glob.glob('/tmp/dmrpp/*.dmrpp')):
+# These large datasets will make program hang.
+skip = ['analysed_sst', 'analysis_error', 'mask', 'sea_ice_fraction']
 
+# Docker mounts /tmp/dmrpp and generates DMR++ there.
+for f in sorted(glob.glob('/tmp/dmrpp/*.dmrpp')):
     ## DAP2
-    #url = 'http://localhost:8080/opendap/' + os.path.basename(f)
+    # url = 'http://localhost:8080/opendap/' + os.path.basename(f)
     
     ## DAP4
     url = 'dap4://localhost:8080/opendap/' + os.path.basename(f)
+
     print(url)
     dataset = open_url(url)
     print(dataset)
     for i in dataset.keys():
         print(i)
-        d = dataset[i][:]
-        print(d)
+        if i not in skip:
+            d = dataset[i][:]
+            print(d)
+        else:
+            print('Skipping '+i)
--- zdset.py
+++ zdset.py
@@ -10,7 +10,7 @@
 
 """
 Read a dataset from Kerchunk using Zarr.
-Compare the dataset values from DataTree-converted Zarr.
+TODO: Compare the dataset values from DataTree-converted Zarr.
 """
 
 import fsspec
@@ -18,18 +18,27 @@ import glob
 import zarr
 
 skip_STARL2P = ['/geostationary', '/crs', '/dt_analysis', '/l2p_flags',
-                '/l3s_sst_reference', '/quality_level',
+                '/quality_level',
                 '/satellite_zenith_angle', '/sea_surface_temperature',
                 '/sses_bias', '/sses_standard_deviation', '/sst_count',
                 '/sst_dtime']
-skip_STARL3S = ['/sst_source', '/wind_speed']
+skip_STARL3S = ['/l3s_sst_reference', '/sst_source', '/wind_speed']
 skip_AMSR = ['/HDFEOS INFORMATION/CoreMetadata.0',
              '/HDFEOS INFORMATION/StructMetadata.0']
 skip_ATL08 = ['/ancillary_data/release', '/ancillary_data/version']
 skip_OMI = ['/HDFEOS INFORMATION/ArchivedMetadata.0']
 skip =  skip_STARL2P + skip_STARL3S + skip_AMSR + skip_ATL08 + skip_OMI
 
-for f in sorted(glob.glob('VNP*.h*5.json')):
+def print_visitor(obj):
+    if type(obj) != zarr.hierarchy.Group:
+        a = obj.name
+        if a in skip:
+            print('Skipping '+a)
+        else:
+            print(a)
+            print(za[a][:])
+                
+for f in sorted(glob.glob('*.h*5.json')):
     print(f)
     mapper = fsspec.get_mapper(
         'reference://',
@@ -38,6 +47,7 @@ for f in sorted(glob.glob('VNP*.h*5.json')):
         remote_protocol='file',
     )
     za = zarr.open(mapper, mode='r')
+    """
     def print_visitor(obj):
         if type(obj) != zarr.hierarchy.Group:
             a = obj.name
@@ -46,7 +56,5 @@ for f in sorted(glob.glob('VNP*.h*5.json')):
             else:
                 print(a)
                 print(za[a][:])
+    """
     za.visitvalues(print_visitor)
-    
-# z5 = zarr.open('ATL08dt.h5.zarr')
-# print(z5['ds_geosegments'][:])
