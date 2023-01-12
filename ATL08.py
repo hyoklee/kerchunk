315357d0827869792c662007a3c2c2eb393c80bc
H. Joe Lee <hyoklee@hdfgroup.org>
2023-01-12 17:41:02 -0600

    Update ATL08.py

--- ATL08.py
+++ ATL08.py
@@ -22,11 +22,11 @@ import xarray as xr
 so = dict(anon=True, default_fill_cache=False, default_cache_type='first')
 
 ## Relative path works fine.
-f = './data/ATL08_20181014084920_02400109_003_01.h5'
+# f = './data/ATL08_20181014084920_02400109_003_01.h5'
 ## Full path also works.
 # f = 'file:///Users/hyoklee/src/kerchunk/data/ATL08_20181014084920_02400109_003_01.h5'
 ## IPFS works fine, too.
-# f = 'ipfs://QmVZc4TzRP7zydgKzDX7CH2JpYw2LJKkWBm6jhCfigeon6'
+f = 'ipfs://QmVZc4TzRP7zydgKzDX7CH2JpYw2LJKkWBm6jhCfigeon6'
 fs2 = fsspec.filesystem('')  #local file system to save final jsons to
 
 with fsspec.open(f, **so) as inf:
